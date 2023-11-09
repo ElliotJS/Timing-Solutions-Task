f8cf8f0d2216ec954ae9efcd19041c474fdb7691
Elliot Smith <elliot@elliotjs.co.uk>
2023-11-09 19:43:50 +0000

    React site getting websocket data

--- src/frontend/client/src/App.js
+++ src/frontend/client/src/App.js
@@ -1,12 +1,44 @@
+import { useState, useEffect, useRef } from "react";
 import { Button } from "react-native";
 
 import "./App.css";
 
 function App() {
+  const [data, SetData] = useState("");
+  const webSocket = useRef(null);
+
+  useEffect(() => {
+    webSocket.current = new WebSocket("wss://localhost:44316/ws");
+
+    webSocket.current.onopen = () => {
+      console.log("Connected to websocket");
+    };
+
+    webSocket.current.onmessage = (event) => {
+      const response = event.data;
+      console.log(response);
+      SetData(response);
+    };
+
+    webSocket.current.onclose = () => {
+      console.log("Disconnected from websocket");
+    };
+
+    webSocket.current.onerror = (error) => {
+      console.log(error);
+    };
+
+    const webSocketCurrent = webSocket.current;
+
+    return () => {
+      webSocketCurrent.close();
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Websocket Data</p>
+        <p>{data}</p>
         <Button title="Close Connection"></Button>
       </header>
     </div>
