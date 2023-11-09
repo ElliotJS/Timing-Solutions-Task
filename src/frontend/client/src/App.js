0302b6aaf05a7e319f2d5129593c59f62c2b8307
Elliot Smith <elliot@elliotjs.co.uk>
2023-11-09 19:38:01 +0000

    Remove unnecessary create-react-app leftovers

---
+++ src/frontend/client/src/App.js
@@ -0,0 +1,16 @@
+import { Button } from "react-native";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Websocket Data</p>
+        <Button title="Close Connection"></Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
